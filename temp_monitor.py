c3dc9dbb953d70d9a2a92fc19d4985aa54c3d618
eusoubrasileiro <eusoubrasileiro@gmail.com>
2024-04-01 19:19:51 -0300

    printing to sys.stderr to see on journal

--- temp_monitor.py
+++ temp_monitor.py
@@ -9,9 +9,10 @@ pip install pyserial pandas
 
 """
 
-import pandas as pd 
+import sys 
 import time
 from datetime import datetime
+import pandas as pd 
 import sqlite3
 import temper 
 
@@ -25,9 +26,10 @@ temp = temper.Temper()
 while True:    
     results = temp.read()[0]
     temp_in, temp_out = results['internal temperature'], results['external temperature']
+    print('temp_in {} temp_out {}'.format(temp_in, temp_out), file=sys.stderr)
     now = datetime.now()
     with sqlite3.connect(dbfile) as conn:
         cursor = conn.cursor()
         cursor.execute(f"INSERT INTO home (time, temp_out, temp_in) VALUES (?, ?, ?)", (datetime.now(), float(temp_out), float(temp_in)))
         conn.commit()    
-    time.sleep(60*5) # every 5 minutes
+    time.sleep(30) # every 30 seconds
